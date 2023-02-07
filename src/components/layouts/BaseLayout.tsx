61095721b627f57c3edbd728000f858bcfb7eccc
SeongWooHong <pluto0303@naver.com>
2023-02-07 17:56:35 +0900

    test

--- src/components/layouts/BaseLayout.tsx
+++ src/components/layouts/BaseLayout.tsx
@@ -22,7 +22,7 @@ const LayoutWrapper = styled.div`
   top: 0;
   left: 0cm;
   width: 100%;
-  height: 100px;
+  height: 80px;
   background-color: black;
   display: flex;
   justify-content: center;
